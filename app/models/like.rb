88dbd7fbbb076e5f3a1490416e05d23b7486275a
Cosmas Wasswa <cosywas@gmail.com>
2023-09-18 23:09:09 +0300

    Fix linter bugs

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
-    belongs_to :post
-    belongs_to :user
+  belongs_to :post
+  belongs_to :user
 
-    def update_post_likes_counter
-        post.update(likes_counter: post.likes.count)
-      end
+  def update_post_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
