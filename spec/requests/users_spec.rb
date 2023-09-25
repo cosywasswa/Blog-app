91cd20f2f264b3ff32754adf8bded11b671e76a6
Cosmas Wasswa <cosywas@gmail.com>
2023-09-25 12:13:18 +0300

    Fixed linter bugs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,43 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe 'Users', type: :request do
   describe 'GET/index' do
-
-   before do
-    get users_url
-  end
+    before do
+      get users_url
+    end
 
     it 'repond with http success' do
-    expect(response.status).to eq(200)
+      expect(response.status).to eq(200)
     end
 
     it 'renders the correct template' do
       expect(response).to render_template('users/index')
-  end
+    end
 
     it 'responds with the correct body' do
       expect(response.body).to include('List of Users')
     end
   end
 
-#show action
+  # show action
   describe 'GET/show' do
-
     before do
       get user_url(1)
     end
 
     it 'repond with http success' do
-    expect(response.status).to eq(200)
+      expect(response.status).to eq(200)
     end
-    
+
     it 'renders the correct template' do
       expect(response).to render_template('users/show')
-  end
+    end
 
     it 'responds with the correct body' do
       expect(response.body).to include('Details of a Selected User')
     end
   end
 end
-
