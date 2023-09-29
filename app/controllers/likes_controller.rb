d895f41a4397ca087966e966026b358501a1c4a3
Cosmas Wasswa <cosywas@gmail.com>
2023-09-29 15:04:56 +0300

    Fix linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,15 +3,15 @@ class LikesController < ApplicationController
     author = User.find(params[:user_id])
     post = Post.find(params[:post_id])
 
-      like = Like.new(user_id: author.id, post_id: post.id)
+    like = Like.new(user_id: author.id, post_id: post.id)
 
-      if like.save
-        post.update(likes_counter: post.likes.count)
-        flash[:success] = 'Like added'
+    if like.save
+      post.update(likes_counter: post.likes.count)
+      flash[:success] = 'Like added'
 
-      else
-        flash[:error] = 'Oops, something went wrong'
-      end
+    else
+      flash[:error] = 'Oops, something went wrong'
+    end
 
     redirect_to user_post_path(author, post)
   end
