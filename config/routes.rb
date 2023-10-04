9efa24ffbb62627417b3d84445d2c92d32a0e09d
Cosmas Wasswa <cosywas@gmail.com>
2023-10-04 18:11:00 +0300

    Added post deletion action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :new, :create, :show] do
+    resources :posts, only: [:index, :new, :create, :show, :destroy] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:new, :create]
     end
