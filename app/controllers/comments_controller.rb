4d759b6bb1e99d4dec08cccdf0b3bfb7439e8e4b
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-10-04 21:03:14 +0200

    Add edit comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,4 +21,18 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text)
   end
+
+  def destroy
+    comment = Comment.find(params[:post_id])
+    authorize! :destroy, post
+    if post.destroy
+      flash.now[:success] = 'Comment was successfully deleted'
+      redirect_to user_post_path(user_id: @post.author_id, post_id: @post.id, id: @post.id)
+    else
+      puts "Couldn't delete post"
+      flash.now[:error] = 'Oops. Could not delete the Comment'
+      redirect_to user_post_path(user_id: @post.author_id, post_id: @post.id, id: @post.id)
+    end
+  end
+
 end
